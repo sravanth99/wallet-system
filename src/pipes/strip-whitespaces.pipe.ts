5c8a4acf64f7a5dba61dee30e95083a4aa908dcb
SravanthAlpfa <sravanth.reddy@alpfaedu.com>
2023-07-20 23:52:26 +0530

    defined pipes

---
+++ src/pipes/strip-whitespaces.pipe.ts
@@ -0,0 +1,11 @@
+import { PipeTransform, Injectable } from '@nestjs/common';
+
+@Injectable()
+export class StripWhitespacesPipe implements PipeTransform {
+  transform(value: any) {
+    if (value && typeof value === 'string') {
+      return value.trim();
+    }
+    return value;
+  }
+}
