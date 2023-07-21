3a79d949433d15c3740d1f0aed4e5bd7d246709d
sravanth99 <sravanth.unity@gmail.com>
2023-07-21 12:33:15 +0530

    added documentation for utils

--- src/utils/trim-whitespaces.ts
+++ src/utils/trim-whitespaces.ts
@@ -1,3 +1,12 @@
+/**
+ * Trims leading and/or trailing white spaces of a string.
+ * @param {string} value - The input string to trim.
+ * @param {('start' | 'end')} [trim] - `Optional`
+ * Specify `start` to trim leading white spaces\
+ *  `end` to trim trailing white spaces\
+ * or omit to trim both.
+ * @returns {string} - The trimmed string.
+ */
 export const trimWhiteSpaces = (value: string, trim?: 'start' | 'end') => {
   if (trim === 'start') return value.trimStart();
   if (trim == 'end') return value.trimEnd();
--- src/utils/validate-object-id.ts
+++ src/utils/validate-object-id.ts
@@ -1,6 +1,13 @@
 import { BadRequestException } from '@nestjs/common';
 import { isValidObjectId } from 'mongoose';
 
+/**
+ * Validates if a string value is a valid MongoDB ObjectId.
+ * @param {string} value - The input value to validate.
+ * @param {string} name - The name of the field being validated (used for error message).
+ * @returns {string} - The valid ObjectId value.
+ * @throws {BadRequestException} - Throws a BadRequestException if the value is not a valid ObjectId.
+ */
 export const validateObjectId = (value: string, name: string) => {
   if (isValidObjectId(value)) {
     return value;
